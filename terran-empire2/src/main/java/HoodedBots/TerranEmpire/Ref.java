0fec49bdf54dbf407eb180c2acd14df735c1f7b5
Brand0n1 <35469559+Brand0n1@users.noreply.github.com>
2018-01-15 13:49:52 -0800

    Add files via upload

---
+++ terran-empire2/src/main/java/HoodedBots/TerranEmpire/Ref.java
@@ -0,0 +1,10 @@
+package HoodedBots.TerranEmpire;
+
+public class Ref {
+	
+	public static final String TOKEN = "NDAyNTY2MDQ3MzA1MTcwOTgx.DT6mYA.ZXPyDb4cPVLkscPz7tqhrmU9dds";
+	public static final String CLIENT_ID= "402566047305170981";
+	public static final String PREFIX = "/";
+
+}
+
